d913ebf22a809108e7bae2f4abd6d2a688079f0f
mariah <riahriah4@gmail.com>
2021-09-16 13:04:03 -0400

    css framework

--- packages/client/src/pages/HomePage.js
+++ packages/client/src/pages/HomePage.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { useApiFetch } from "util/api"
-import LoadingSpinner from '../components/LoadingSpinner'
+import React from "react";
+import { useApiFetch } from "util/api";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function HomePage(props) {
-  const {error, isLoading, response} = useApiFetch("/sample")
+  const { error, isLoading, response } = useApiFetch("/sample");
 
   return (
     <main>
-      <h1>Welcome to my app!</h1>
-      { error && <h3 style={{color:"red"}}>Error Loading Data: {error}</h3>}
-      { isLoading &&  <LoadingSpinner></LoadingSpinner>}
-      { !error && response && (
-        <div>Username: {response.username}</div>
+      {error && <h3 style={{ color: "red" }}>Error Loading Data: {error}</h3>}
+      {isLoading && <LoadingSpinner></LoadingSpinner>}
+      {!error && response && (
+        <div>
+          Username: {response.username}{" "}
+          <button class="btn" id="logout">
+            Log Out
+          </button>
+        </div>
       )}
+      <h1>Welcome to GameTrak!</h1>
+      <button>Login</button> <button>Sign Up</button>
     </main>
-  )
+  );
 }
