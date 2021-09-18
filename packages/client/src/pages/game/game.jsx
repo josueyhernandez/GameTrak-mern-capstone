79cab340123d131e959d7803413d55141d9ef41d
jsmith1939 <60932568+jsmith1939@users.noreply.github.com>
2021-09-18 19:09:11 -0400

    Created game file and data file that stores a list of games along with images

---
+++ packages/client/src/pages/game/game.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import GameData from './data';
+
+class GamesPage extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			collections: GameData,
+		};
+	}
+	render() {
+		const { collections } = this.state;
+		return (
+			<div className='game-page'>{collections.map({ id, ...collections })}</div>
+		);
+	}
+}
+
+export default GamesPage;
