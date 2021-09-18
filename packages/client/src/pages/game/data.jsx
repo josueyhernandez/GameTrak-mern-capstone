0fe77f81da51b7b72fbd3f79432e8bfdd6743b54
jsmith1939 <60932568+jsmith1939@users.noreply.github.com>
2021-09-18 19:09:11 -0400

    Created game file and data file that stores a list of games along with images

---
+++ packages/client/src/pages/game/data.jsx
@@ -0,0 +1,53 @@
+const gameData = {
+	title: 'games',
+	type: [
+		{
+			id: 1,
+			name: 'Horizon Zero Dawn',
+			imageUrl:
+				'https://gamepost.ua/img/horizon-forbidden-west-ps5-00-gamepost-ua.jpg',
+		},
+		{
+			id: 2,
+			name: 'ZELDA BREATH OF THE WILD',
+			imageUrl:
+				'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5721/5721500_sd.jpg',
+		},
+		{
+			id: 3,
+			name: 'GOW RAGNAROK',
+			imageUrl:
+				'https://th.bing.com/th/id/R.72349fe208981a77a80dfe26f2446290?rik=q5sB5vTT3CbotA&riu=http%3a%2f%2fwww.sotcomm.ru%2fwp-content%2fuploads%2f2021%2f01%2fgod-of-war-ragnarok-ps5_1.jpg&ehk=u%2f%2btBNacKeohnu7bLf87b%2fa%2b0HfnuAALXxdSj%2b6fBnM%3d&risl=&pid=ImgRaw&r=0',
+		},
+		{
+			id: 4,
+			name: 'SPIDER-MAN',
+			imageUrl:
+				'https://media.gamestop.com/i/gamestop/11096536/Marvels-Spider-Man-Game-of-the-Year-Edition---PlayStation-4?$pdp2x$',
+		},
+		{
+			id: 5,
+			name: 'SPIDER-MAN MILES MORALES',
+			imageUrl:
+				'https://media.gamestop.com/i/gamestop/11108924/Marvels-Spider-Man-Miles-Morales---PlayStation-4?$pdp2x$',
+		},
+		{
+			id: 6,
+			name: '2K21',
+			imageUrl:
+				'https://media.gamestop.com/i/gamestop/11151961/NBA-2K22---PlayStation-4?$pdp2x$$&fmt=webp',
+		},
+		{
+			id: 7,
+			name: '2K21',
+			imageUrl:
+				'https://media.gamestop.com/i/gamestop/11151961/NBA-2K22---PlayStation-4?$pdp2x$$&fmt=webp',
+		},
+		{
+			id: 7,
+			name: 'DRAGONBALL Z KAKAROT',
+			imageUrl:
+				'https://media.gamestop.com/i/gamestop/10172926/DRAGON-BALL-Z-KAKAROT---Xbox-One?$pdp2x$$&fmt=webp',
+		},
+	],
+};
