b345888551f43cac402612eb32378bd71fae14e0
Austin Bond <bondaustin80@gmail.com>
2021-09-20 10:33:02 -0400

    rebasing -m

--- packages/client/src/pages/homepage/HomePage.js
+++ packages/client/src/pages/homepage/HomePage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
@@ -7,17 +6,6 @@ import { useApiFetch } from "util/api"
 import LoadingSpinner from 'components/LoadingSpinner'
 import { FaExclamationCircle } from 'react-icons/fa'
 import "./HomePage.css" 
-=======
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-// import { ToastContainer, toast } from "react-toastify"
-import { useApiFetch } from "util/api";
-import LoadingSpinner from "components/LoadingSpinner";
-import { FaExclamationCircle } from "react-icons/fa";
-import "../index.css";
-import "./HomePage.css";
->>>>>>> 6404c0f (rebased changes)
 
 export default function HomePage(props) {
   const { error, isLoading, response } = useApiFetch("/sample");
@@ -57,21 +45,7 @@ export default function HomePage(props) {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </Form.Group>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> cd50266 (rebasing -m)
-=======
-<<<<<<< HEAD
-=======
->>>>>>> d2356f6 (rebasing)
->>>>>>> e809760 (rebasing -m)
-=======
-
->>>>>>> 6404c0f (rebased changes)
             <Button>SignUp</Button>
             <Button
               block
@@ -82,25 +56,6 @@ export default function HomePage(props) {
             >
               Login
             </Button>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <Button block size="lg" type="submit" class="button" disabled={!validateForm()}>Login</Button>
-=======
-=======
-            <Button block size="lg" type="submit" class="button" disabled={!validateForm()}>Login</Button>
->>>>>>> 463250d (Login setup)
->>>>>>> cd50266 (rebasing -m)
-=======
-=======
-            <Button block size="lg" type="submit" class="button" disabled={!validateForm()}>Login</Button>
->>>>>>> 463250d (Login setup)
-=======
->>>>>>> d2356f6 (rebasing)
->>>>>>> e809760 (rebasing -m)
-=======
-            <Button block size="lg" type="submit" class="button" disabled={!validateForm()}>Login</Button>
->>>>>>> 6404c0f (rebased changes)
           </Form>
         </div>
       )}
