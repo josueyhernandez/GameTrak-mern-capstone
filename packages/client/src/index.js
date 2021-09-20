aeade0dc2c62c82a4788b8bd9ebe961e0158d64b
Austin Bond <bondaustin80@gmail.com>
2021-09-20 10:02:19 -0400

    added to index

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import HomePage from 'pages/HomePage'
+import RegisterPage from './pages/RegisterPage'
 import './index.css'
 
 ReactDOM.render(
@@ -9,6 +10,7 @@ ReactDOM.render(
     <BrowserRouter>  
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/register' component={RegisterPage}
         { /* Add more routes here */} 
       </Switch>
     </BrowserRouter>
