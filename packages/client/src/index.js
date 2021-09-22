cee293d8839fe96d1b36ba462405d4c828c181a7
Austin Bond <bondaustin80@gmail.com>
2021-09-22 08:45:04 -0400

    fixed typo

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import HomePage from 'pages/HomePage'
+import HomePage from './pages/homepage/HomePage'
 import RegisterPage from './pages/RegisterPage'
 import './index.css'
 
