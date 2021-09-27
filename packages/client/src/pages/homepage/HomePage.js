aa854e0b61a837e3beb0ec98b7687714210e6f8d
mariah <riahriah4@gmail.com>
2021-09-27 12:07:16 -0400

    saving

--- packages/client/src/pages/homepage/HomePage.js
+++ packages/client/src/pages/homepage/HomePage.js
@@ -1,16 +1,15 @@
-
-import React, { useState } from 'react'
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-// import { ToastContainer, toast } from "react-toastify" 
-import { useApiFetch } from "util/api"
-import LoadingSpinner from 'components/LoadingSpinner'
-import { FaExclamationCircle } from 'react-icons/fa'
-import "./HomePage.css" 
-
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+// import { ToastContainer, toast } from "react-toastify"
+import { useApiFetch } from "util/api";
+import LoadingSpinner from "components/LoadingSpinner";
+import { FaExclamationCircle } from "react-icons/fa";
+// import "../index.css";
+import "./HomePage.css";
 
 export default function HomePage(props) {
-  const axios = require('axios');
+  const axios = require("axios");
   const { error, isLoading, response } = useApiFetch("/sample");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -19,40 +18,46 @@ export default function HomePage(props) {
   function validateForm() {
     return username.length > 0 && password.length > 0;
   }
-  async function loginBackend(){
-    await axios.post('/api/users/login',{
-      username,
-      password
-    }).then(function(res){
-      if(res.data === true){
-        setLoginStatus(2)
-      }else{
-        setLoginStatus(1)
-      }
-    }).catch(function(err){
-      console.log(err)
-    })
-
+  async function loginBackend() {
+    await axios
+      .post("/api/users/login", {
+        username,
+        password,
+      })
+      .then(function (res) {
+        if (res.data === true) {
+          setLoginStatus(2);
+        } else {
+          setLoginStatus(1);
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
   function handleSubmit(event) {
-    console.log(username, password)
+    console.log(username, password);
     loginBackend();
     event.preventDefault();
   }
-  async function testButton(){
-    
-     await axios.get('/api/users/login',{params:{
-      username: "Kitboga",
-      password: "Donotredeem"
-    }}).then(function(res){
-      if(res.data === true){
-        setLoginStatus(2)
-      }else{
-        setLoginStatus(1)
-      }
-    }).catch(function(err){
-      console.log(err)
-    })
+  async function testButton() {
+    await axios
+      .get("/api/users/login", {
+        params: {
+          username: "Kitboga",
+          password: "Donotredeem",
+        },
+      })
+      .then(function (res) {
+        if (res.data === true) {
+          setLoginStatus(2);
+        } else {
+          setLoginStatus(1);
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 
   return (
@@ -81,13 +86,16 @@ export default function HomePage(props) {
               />
             </Form.Group>
 
-            <Button 
-            block
-            size="lg"
-            type="submit"
-            className="button"
-            onClick={() => window.location.replace("/register")}
-            variant="primary">SignUp</Button>
+            <Button
+              block
+              size="lg"
+              type="submit"
+              className="button"
+              onClick={() => window.location.replace("/register")}
+              variant="primary"
+            >
+              SignUp
+            </Button>
             <Button
               block
               size="lg"
