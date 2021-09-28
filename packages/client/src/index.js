18577ae93e41889081b9cff9b45e078902b642cf
Josue Hernandez <josueyhernandez18@gmail.com>
2021-09-28 09:41:00 -0400

    Create Chrarcter page

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -9,24 +9,23 @@ import ItemPage from 'pages/ItemPage/item'
 import AttributePage from 'pages/AttributePage/attribute'
 import PlacesPage from 'pages/PlacesPage/places'
 import SkillsPage from 'pages/SkillsPage/skills'
+import CreatePage from 'pages/CreateCharacter/create'
 import './index.css'
 import ProvideUser from "./hooks/globalStates"
 ReactDOM.render(
   <React.StrictMode>
-    <ProvideUser>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/games' component={GamesPage} />
-          <Route exact path='/register' component={RegisterPage} />
-          <Route exact path="/character" component={CharacterPage} />
-          <Route exact path="/item" component={ItemPage} />
-          <Route exact path="/attributes" component={AttributePage} />
-          <Route exact path="/places" component={PlacesPage} />
-          <Route exact path="/skills" component={SkillsPage} />
-        </Switch>
-      </BrowserRouter>
-    </ProvideUser>
+    <BrowserRouter>  
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/register' component={RegisterPage} />
+        <Route exact path="/character" component={CharacterPage} />
+        <Route exact path="/item" component={ItemPage} />
+        <Route exact path="/attributes" component={AttributePage} />
+        <Route exact path="/places" component={PlacesPage} />
+        <Route exact path="/skills" component={SkillsPage} />
+        <Route exact path="/create" component={CreatePage} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
