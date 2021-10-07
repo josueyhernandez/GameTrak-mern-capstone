ebfbc8c2f69ef237f234e939a32fb3f3814b0ce3
myname <justin.m.wu@protonmail.com>
2021-10-07 10:36:25 -0400

    connected to atlas

--- packages/server/src/config/keys.js
+++ packages/server/src/config/keys.js
@@ -4,7 +4,7 @@ module.exports = {
     apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
   },
   database: {
-    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/MERNApp', // for local mongodb
+    url: 'mongodb+srv://gametraker:gametrak@cluster0.n8pt6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost:27017/MERNApp', // for local mongodb
     // url: "PUT-YOUR-MONGODB-CONNECTION-STRING-HERE" // for mongodb atlas, comment the above line, uncomment this line, and add your connection string.
   },
   jwt: {
