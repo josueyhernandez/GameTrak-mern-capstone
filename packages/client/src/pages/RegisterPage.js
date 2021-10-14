8515e8ab646f3ed3f78dcae9e897d4c27a792d36
Austin Bond <bondaustin80@gmail.com>
2021-10-14 10:10:13 -0400

    Fixed glitch:

--- packages/client/src/pages/RegisterPage.js
+++ packages/client/src/pages/RegisterPage.js
@@ -83,7 +83,7 @@ export default function RegisterPage() {
 
   }
   async function submitRegister() {
-    if (!data.username.includes(" ") || data.username.length() <= 20) {
+    if (!data.username.includes(" ") && data.username.length <= 20) {
       await axios.post('/api/users', {
         username: data.username,
         password: data.password,
